2285fb0f57d2b2b68be37c03d5f9724b1deaff17
leon <leonz5288@hotmail.com>
2021-05-28 20:04:18 +0800

    work

--- python/build.py
+++ python/build.py
@@ -11,7 +11,8 @@ def get_os_name():
     name = platform.platform()
     # in python 3.8, platform.platform() uses mac_ver() on macOS
     # it will return 'macOS-XXXX' instead of 'Darwin-XXXX'
-    if name.lower().startswith('darwin')           or name.lower().startswith('macos'):
+
+    if name.lower(              ).startswith('darwin')           or name.lower().startswith('macos'):
         return 'osx'
     elif name.lower().startswith('windows'):
         return 'win'
