92b98dd3f7106e2801a1bba7af99b309dc62227e
yolo2themoon <markbench@163.com>
2021-10-18 21:12:32 +0800

    sys.argv[1] path to store

--- benchmarks/misc/run.py
+++ benchmarks/misc/run.py
@@ -1,4 +1,5 @@
 import datetime
+import sys
 
 from membound import Membound
 from taichi.core import ti_core as _ti_core
@@ -21,25 +22,24 @@ class PerformanceMonitoring:
         for s in self.suites:
             s.run()
 
-    def write_md(self, path='/home/benchmarkbot/benchmark/'):
+    def write_to_path(self, path_with_file_name='./performance_result.md'):
+        with open(path_with_file_name, 'w') as f:
+            for arch in test_archs:
+                for s in self.suites:
+                    lines = s.mdlines(arch)
+                    for line in lines:
+                        print(line, file=f)
+
+    def store_with_date_and_commit_hash(self, path='./'):
         current_time = datetime.datetime.now().strftime("%Y%m%dd%Hh%Mm%Ss")
         commit_hash = _ti_core.get_commit_hash()[:8]
         filename = f'perfresult_{current_time}_{commit_hash}.md'
-        #format like perfresult_20211013d20h46m03s_581fdac8.md
-        with open(path + filename, 'w') as f:
-            for arch in test_archs:
-                for s in self.suites:
-                    lines = s.mdlines(arch)
-                    for line in lines:
-                        print(line, file=f)
-        with open('performance_result.md', 'w') as f:  # for /benchamark
-            for arch in test_archs:
-                for s in self.suites:
-                    lines = s.mdlines(arch)
-                    for line in lines:
-                        print(line, file=f)
+        print('store to: ' + path + filename)
+        self.write_to_path(path + filename)
 
 
+path_to_store = sys.argv[1] if len(sys.argv) > 1 else './'
 p = PerformanceMonitoring()
 p.run()
-p.write_md()
+p.write_to_path()  # for /benchamark
+p.store_with_date_and_commit_hash(path_to_store)  #for post-submit
