c190bbc40ab79c589952c73da5e952468b601f05
yolo2themoon <markbench@163.com>
2021-10-18 20:20:22 +0800

    format

--- benchmarks/misc/run.py
+++ benchmarks/misc/run.py
@@ -21,19 +21,19 @@ class PerformanceMonitoring:
         for s in self.suites:
             s.run()
 
-    def write_md(self,path='/home/benchmarkbot/benchmark/'):
+    def write_md(self, path='/home/benchmarkbot/benchmark/'):
         current_time = datetime.datetime.now().strftime("%Y%m%dd%Hh%Mm%Ss")
         commit_hash = _ti_core.get_commit_hash()[:8]
         filename = f'perfresult_{current_time}_{commit_hash}.md'
         #format like perfresult_20211013d20h46m03s_581fdac8.md
         #TODO: get pr id and add to file name perfresult_pr3104_20211013d20h46m03s_581fdac8.md
-        with open(path+filename, 'w') as f:
+        with open(path + filename, 'w') as f:
             for arch in test_archs:
                 for s in self.suites:
                     lines = s.mdlines(arch)
                     for line in lines:
                         print(line, file=f)
-        with open('performance_result.md', 'w') as f: # for /benchamark
+        with open('performance_result.md', 'w') as f:  # for /benchamark
             for arch in test_archs:
                 for s in self.suites:
                     lines = s.mdlines(arch)
