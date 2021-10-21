1ac11ae20d8bff38d6d1e7d2aa01ff504e72cb01
Qiao Bo <boqiao@taichi.graphics>
2021-10-21 22:10:52 +0800

    Remove user flag

--- .github/workflows/scripts/unix_docker_build.sh
+++ .github/workflows/scripts/unix_docker_build.sh
@@ -18,9 +18,9 @@ source /home/dev/miniconda/etc/profile.d/conda.sh
 conda activate $PY
 
 cd taichi
-python3 -m pip install --user -r requirements_dev.txt
+python3 -m pip install -r requirements_dev.txt
 python3 -m pip install torch==1.9.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
-TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS python3 setup.py install --user
+TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS python3 setup.py install 
 
 export TI_IN_DOCKER=true
 
