cebc5ed34756e34e218ef11b3561af27c9c7836c
yolo2themoon <markbench@163.com>
2021-11-01 18:37:19 +0800

    a

--- benchmarks/misc/print.py
+++ benchmarks/misc/print.py
@@ -1,15 +1,13 @@
-
-
-
-
-import sys
-import os
 import json
+import os
 import re
+import sys
+
 
 def main():
     num = os.environ.get('PULL_REQUEST_NUMBER')
     print(f'PULL_REQUEST_NUMBER = {num}')
+
 
 if __name__ == '__main__':
     main()
--- benchmarks/misc/test.py
+++ benchmarks/misc/test.py
@@ -1,17 +1,17 @@
-
-import sys
-import os
 import json
+import os
 import re
+import sys
+
 
 def main():
     github_context = os.environ.get('GITHUB_CONTEXT')
     github_info_dict = {}
     github_info_dict = json.loads(github_context)
-    pr_number = re.sub("\D","",github_info_dict["ref"])
-    set_pr_num_str = f'export PULL_REQUEST_NUMBER={pr_number}'
-    print(set_pr_num_str)
-    os.system(set_pr_num_str)
+    pr_number = re.sub("\D", "", github_info_dict["ref"])
+    os.environ['PULL_REQUEST_NUMBER'] = pr_number
+    print(os.environ.get('PULL_REQUEST_NUMBER'))
+
 
 if __name__ == '__main__':
     main()
