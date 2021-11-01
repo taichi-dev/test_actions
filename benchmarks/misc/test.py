fb992085ff7a2cc9f44535a6db590e0bef3fa998
yolo2themoon <markbench@163.com>
2021-11-01 17:57:47 +0800

    x

--- benchmarks/misc/test.py
+++ benchmarks/misc/test.py
@@ -17,15 +17,11 @@ def dump2json(obj):
 
 def main():
     github_context = os.environ.get('GITHUB_CONTEXT')
-    # info_path = os.path.join(perf_monitoring_dir, 'context.json')
-    #traverse pr_results
-    pr_dict = {}
-    # with open(info_path, 'r') as f:
-    pr_dict = json.loads(github_context)
-    pr_number = re.sub("\D","",pr_dict["ref"])
-    print(pr_dict["ref"])
+    github_info_dict = {}
+    github_info_dict = json.loads(github_context)
+    pr_number = re.sub("\D","",github_info_dict["ref"])
+    print(github_info_dict["ref"])
     print(pr_number)
-    # print(dump2json(pr_dict))
 
 if __name__ == '__main__':
     main()
