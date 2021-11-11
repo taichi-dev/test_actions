fd66ffa0fb0ad6d73719772986d2b38533c98eb6
Yuheng Zou <zouyuheng1998@gmail.com>
2021-11-11 14:15:12 +0800

    Test rebase fail

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ classifiers = [
 
 project_name = os.getenv('PROJECT_NAME', 'taichi')
 TI_VERSION_MAJOR = 0
-TI_VERSION_MINOR = 8
-TI_VERSION_PATCH = 3
+TI_VERSION_MINOR = 6
+TI_VERSION_PATCH = 8
 version = f'{TI_VERSION_MAJOR}.{TI_VERSION_MINOR}.{TI_VERSION_PATCH}'
 
 data_files = glob.glob('python/lib/*')
