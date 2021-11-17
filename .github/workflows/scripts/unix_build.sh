a59c3794ddfe39c116c865dc817d2c2232c96a92
Frost Ming <mianghong@gmail.com>
2021-11-17 16:27:13 +0800

    Remove usage of build.py in favor of setup.py

--- .github/workflows/scripts/unix_build.sh
+++ .github/workflows/scripts/unix_build.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
 set -ex
-export PATH=`pwd`/taichi-llvm/bin/:$LLVM_PATH:$PATH
+
 python3 -m pip uninstall taichi taichi-nightly -y
 python3 -m pip install -r requirements_dev.txt
-if [[ $OSTYPE == 'darwin'* ]]; then brew install molten-vk; fi
-cd python
+python3 -m pip install -r requirements_test.txt
 git fetch origin master
-TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS python3 build.py build
-cd ..
+python3 setup.py bdist_wheel
+
 export NUM_WHL=`ls dist/*.whl | wc -l`
 if [ $NUM_WHL -ne 1 ]; then echo `ERROR: created more than 1 whl.` && exit 1; fi
 python3 -m pip install dist/*.whl
